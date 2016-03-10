00f51e76de687b5386f5c2b2a00afa03cb65ef5a
Xingw <541460835@qq.com>
2016-03-10 20:54:30 +0800

    添加网络获取接口

---
+++ app/src/main/java/com/huster/xingw/autowall/Model/GoodsResult.java
@@ -0,0 +1,7 @@
+package com.huster.xingw.autowall.Model;
+
+/**
+ * Created by Xingw on 2016/3/10.
+ */
+public class GoodsResult {
+}
---
+++ app/src/main/java/com/huster/xingw/autowall/Net/GankCloudApi.java
@@ -0,0 +1,102 @@
+package com.huster.xingw.autowall.Net;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.huster.xingw.autowall.MainActivity;
+import com.huster.xingw.autowall.Model.GoodsResult;
+import com.squareup.okhttp.Cache;
+import com.squareup.okhttp.OkHttpClient;
+
+import java.io.File;
+import java.io.IOException;
+
+import retrofit.RequestInterceptor;
+import retrofit.RestAdapter;
+import retrofit.client.OkClient;
+import retrofit.converter.GsonConverter;
+import retrofit.http.GET;
+import retrofit.http.Path;
+import rx.Observable;
+
+/**
+ * Created by Xingw on 2016/3/10.
+ */
+public class GankCloudApi {
+
+    public static GankCloudApi instance;
+
+    public static GankCloudApi getIns() {
+        if (null == instance) {
+            synchronized (GankCloudApi.class) {
+                if (null == instance) {
+                    instance = new GankCloudApi();
+                }
+            }
+        }
+        return instance;
+    }
+
+    private final GankCloudService mWebService;
+    public static Gson gson = new GsonBuilder().setDateFormat("yyyy-MM-dd HH:mm:ss").create();
+
+    public GankCloudApi() {
+        Cache cache;
+        OkHttpClient okHttpClient = null;
+        try {
+            File cacheDir = new File(MainActivity.getContext().getCacheDir().getPath(),
+                    "gank_cache.json") ;
+            cache = new Cache(cacheDir,10 * 1024 * 1024);
+            okHttpClient = new OkHttpClient();
+            okHttpClient.setCache(cache);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        RestAdapter restAdapter = new RestAdapter.Builder()
+                .setEndpoint("http://gank.avosapps.com/api")
+                .setClient(new OkClient(okHttpClient))
+                .setConverter(new GsonConverter(gson))
+                .setRequestInterceptor(mRequestInterceptor)
+                .build();
+        mWebService = restAdapter.create(GankCloudService.class);
+    }
+
+    private RequestInterceptor mRequestInterceptor = new RequestInterceptor() {
+        @Override
+        public void intercept(RequestFacade request) {
+            request.addHeader("Cache-Control", "public, max-age=" + 60 * 60 * 4);
+            request.addHeader("Content-Type", "application/json");
+        }
+    };
+
+    public interface GankCloudService {
+
+        @GET("/data/Android/{limit}/{page}")
+        Observable<GoodsResult> getAndroidGoods(
+                @Path("limit") int limit,
+                @Path("page") int page
+        );
+
+        @GET("/data/iOS/{limit}/{page}")
+        Observable<GoodsResult> getIosGoods(
+                @Path("limit") int limit,
+                @Path("page") int page
+        );
+
+        @GET("/data/all/{limit}/{page}")
+        Observable<GoodsResult> getAllGoods(
+                @Path("limit") int limit,
+                @Path("page") int page
+        );
+
+        @GET("/data/福利/{limit}/{page}")
+        Observable<GoodsResult> getBenefitsGoods(
+                @Path("limit") int limit,
+                @Path("page") int page
+        );
+    }
+    public Observable<GoodsResult> getAllGoods(int limit, int page){
+        return mWebService.getAllGoods(limit,page);
+    }
+
+}
